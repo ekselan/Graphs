30b1996b3c9b6685c47fb333938d172cfe7093e8
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-08-13 17:02:38 -0400

    class IV

--- class_IV/floodfill.py
+++ class_IV/floodfill.py
@@ -1,19 +1,20 @@
+import sys
+import time
+
 # The floodfill algorithm will fill any enclosed shape(s) with
 # a color
 
 image = [
-    """
-    "....########........"
-    "....#......#........"
-    "....#......#........"
-    "....#..#######......"
-    "....#..#.....#......"
-    "....####.....######."
-    "....#.............#."
-    "....###############."
-    "...................."
-    "...................."
-    """
+    list("....########........"),
+    list("....#......#........"),
+    list("....#......#........"),
+    list("....#..#######......"),
+    list("....#..#.....#......"),
+    list("....####.....######."),
+    list("....#.............#."),
+    list("....###############."),
+    list("...................."),
+    list("....................")
 ]
 
 def print_image():
@@ -21,7 +22,31 @@ def print_image():
         print("".join(line))
 
 def floodfill(row, col, c):
-    floodfill(...)
+    if row < 0 or row > len(image) -1 or col < 0 or col > len(image[0]) - 1:
+        return
 
-print_image()
-floodfill(2,5, "*")
+    if image[row][col] != ".":
+        return
+
+    image[row][col] = c
+
+    sys("clear")
+    print_image()
+    time.sleep(0.25)
+
+    # north neighbor
+    floodfill(row-1, col, c)
+    # south
+    floodfill(row+1, col, c)
+    # east
+    floodfill(row, col+1, c)
+    # west
+    floodfill(row, col-1, c)
+
+if __name__ == "__main__":
+    
+    floodfill(2, 5, "*")
+    # print_image()
+    # print("---" * 10)
+    # floodfill(1, 1, "$")
+    # print_image()
